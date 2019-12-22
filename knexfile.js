5e89b04ed807b3a3e476bfb060cd58bf834e0cc6
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-22 10:46:53 -0800

    preparing to deploy

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,44 @@
 module.exports = {
     development: {
-      client: 'sqlite3',
-      connection: { filename: './database/market.db3' },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-        tableName: 'dbmigrations',
-      },
-      seeds: { directory: './database/seeds' },
+        client: 'sqlite3',
+        connection: { filename: './database/market.db3' },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './database/migrations',
+            tableName: 'dbmigrations',
+        },
+        seeds: { directory: './database/seeds' },
+        pool: {
+            afterCreate: function (connection, done) {
+                connection.run('PRAGMA foreign_keys = ON', done)
+            }
+        }
     },
     testing: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      }
+        client: 'sqlite3',
+        connection: {
+            filename: './database/test.db3',
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './database/migrations',
+        },
+        seeds: {
+            directory: './database/seeds',
+        }
+    },
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './database/migrations'
+        },
+        seeds: {
+            directory: './database/seeds',
+        }
     }
-  };
+};
