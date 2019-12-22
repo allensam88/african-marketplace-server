46964b36a67f159debf8f70dfda5a221778e3378
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-22 13:55:16 -0800

    annoying deploy issues with env

--- knexfile.js
+++ knexfile.js
@@ -26,5 +26,19 @@ module.exports = {
         seeds: {
             directory: './database/seeds',
         }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './data/migrations'
+        },
+        seeds: {
+            directory: './data/seeds'
+        }
     }
 };
