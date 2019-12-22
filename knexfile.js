8edcb2a28e99c0383ee8bb08fec72a3e0da8aecc
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-21 18:24:54 -0800

    added seed data

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,25 @@
 module.exports = {
     development: {
-        client: 'sqlite3',
-        useNullAsDefault: true,
-        connection: {filename: './database/market.db3'},
-        migrations: {directory: './database/migrations'},
-        seeds: {directory: './database/seeds'},
-        pool: {
-            afterCreate: (conn, done) => {conn.run('PRAGMA foreign_keys = ON', done);}
-        }
+      client: 'sqlite3',
+      connection: { filename: './database/market.db3' },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+        tableName: 'dbmigrations',
+      },
+      seeds: { directory: './database/seeds' },
     },
     testing: {
-        client: 'sqlite3',
-        useNullAsDefault: true,
-        connection: {filename: './database/test.db3'},
-        migrations: {directory: './database/migrations'},
-        seeds: {directory: './database/seeds'}
+      client: 'sqlite3',
+      connection: {
+        filename: './database/test.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      }
     }
-};
+  };
