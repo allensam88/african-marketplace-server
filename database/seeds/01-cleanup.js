6073ff84b4b799241375d26702475c1b002a23ad
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-23 10:41:51 -0800

    added restart identity to knex-cleaner

--- database/seeds/01-cleanup.js
+++ database/seeds/01-cleanup.js
@@ -3,6 +3,7 @@ const cleaner = require('knex-cleaner');
 exports.seed = function(knex) {
     return cleaner.clean(knex, {
         mode: 'truncate',
+        restartIdentity: true,
         ignoreTables: ['dbmigrations', 'dbmigrations_lock'],
     });
 };
